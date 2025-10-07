7433effe6018d387f3b10d989180e0c29a0b51e8
iabelleirav24 <iabelleirav@fpcoruna.afundacion.org>
2025-10-07 13:16:20 +0200

    Eliminado el código de Django de la rama Android

--- DjangoPFC/OcioNProject/OcioNProject/MiOcioNApp/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from .models import AppUser, Local, Review, Evento
-
-admin.site.register(AppUser)
-admin.site.register(Local)
-admin.site.register(Review)
-admin.site.register(Evento)
--- DjangoPFC/OcioNProject/OcioNProject/MiOcioNApp/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class MiocionappConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'MiOcioNApp'
