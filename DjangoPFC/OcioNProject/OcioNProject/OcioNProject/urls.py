3765d93dca1c1fa5b483ff0918dab044d6304e6c
iabelleirav24 <iabelleirav@fpcoruna.afundacion.org>
2025-10-07 12:58:42 +0200

    Endpoints para el register y el login creados

--- DjangoPFC/OcioNProject/OcioNProject/OcioNProject/urls.py
+++ DjangoPFC/OcioNProject/OcioNProject/OcioNProject/urls.py
@@ -17,6 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from MiOcioNApp import endpoints
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('users', endpoints.register_user),
+    path('session', endpoints.session),
 ]
